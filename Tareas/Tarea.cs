d77de6faf93c7480e934a5fd4347d04b67b7a0ca
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-06-17 15:44:43 -0300

    Primer commit

---
+++ Tareas/Tarea.cs
@@ -0,0 +1,18 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class Tarea
+{
+    [JsonPropertyName("userId")]
+    public int UserId { get; set; }
+
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("completed")]
+    public bool Completed { get; set; }
+
+}
