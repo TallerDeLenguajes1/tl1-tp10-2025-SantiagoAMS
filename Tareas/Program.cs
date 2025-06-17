99e303184cc0766ce84d74ab7fdeed9ea683e851
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-06-17 16:18:26 -0300

    Se realiza el listado

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+internal class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+    private static async Task Main(string[] args)
+    {
+        var l = await ObtenerTareas();
+        foreach (var t in l)
+        {
+            Console.WriteLine($"{t.Id}: \"{t.Title}\" -> {(t.Completed?"Completada":"No completada")}");
+        }
+    }
+
+    private static async Task<List<Tarea>> ObtenerTareas()
+    {
+        var ret = await client.GetAsync(@"https://jsonplaceholder.typicode.com/todos/");
+        ret.EnsureSuccessStatusCode();
+        string responseBody = await ret.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+
+    }
+
+
+}
