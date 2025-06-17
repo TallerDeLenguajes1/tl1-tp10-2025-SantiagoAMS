8974915ce885685a015d49efed1c73154574c387
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-06-17 16:57:22 -0300

    Punto dos terminado pero requiere tests

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -1,10 +1,22 @@
-﻿internal class Program
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+internal class Program
 {
     private static readonly HttpClient client = new HttpClient();
     private static async Task Main(string[] args)
     {
-
+        var res = await client.GetAsync(@"https://jsonplaceholder.typicode.com/users");
+        res.EnsureSuccessStatusCode();
+        var response = await res.Content.ReadAsStringAsync();
+        var list = JsonSerializer.Deserialize<List<Usuario>>(response);
+        foreach (var u in list)
+        {
+            Console.WriteLine(u.ToString());
+        }
+        var ser = JsonSerializer.Serialize(list);
+        File.WriteAllText(".\\usuarios.json",ser);
     }
     
-    
+
 }
