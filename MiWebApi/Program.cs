a131910b59913359c415a3e25e67517c14c268dd
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-06-25 14:02:15 -0300

    inicio del puntro 3

---
+++ MiWebApi/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    private static Task Main(string[] args)
+    {
+        
+
+
+    }
+
+    private static Task<List<Object>> Obtener(string url, Class c){
+        var ret = await client.GetAsync(@"https://");
+        ret.EnsureSuccessStatusCode();
+        var response = await ret.Content.ReadAsStringAsync();
+    }
+
+}
